21edbf142d6c17999d33e3160f2def758bde9b87
Ethan Povlot <epovlot8589@gmail.com>
2023-10-03 18:50:39 -0400

    update to make website

--- dash_app.py
+++ dash_app.py
@@ -8,15 +8,15 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import spacy
+from collections import Counter
 from time import sleep
+from waitress import serve
 
 nlp = spacy.load("en_core_web_sm")
 df = pd.read_pickle('last_month.pkl') #de_duped, osf_data
 pref_df = pd.read_pickle('user_pref.pkl') ##fix this
 df_searched = df.copy()
-username = ''
 VALID_USERNAME_PASSWORD_PAIRS = {
-   
     'Emory': 'password',
     'GT':'password',
     'Harold':'password'
@@ -37,31 +37,27 @@ def get_records_bool(x, uni_lst):
         if name in str(x):
             return 'True'
     return 'False'
+
 def get_dropdown_options(lst):
+    uni_lst_temp = [item.strip() for val in lst for item in str(val).replace("[", '').replace("]", '').replace("'", "").replace(";", ',').split(',')]
+    flat_list = []
+    known_schools = {'@emory.edu', '@gatech.edu', 'Georgia Institute of Technology', 'gatech'}
 
-    uni_lst_temp = []
-    for val in lst:
-        for uni in str(val).replace("[", '').replace("]", '').replace("'","").replace(";", ',').split(', '):
-            uni_lst_temp.append(uni.strip())
-    out = []
-    for name in uni_lst_temp:
-        if len(name)<3:
-            continue
-        if '@' in name:
-            email = name[name.find('@'):]
-            if '.edu' in email:
-                email = email[:email.find('.edu')+4]
-            out.append(email)
-        else:
-            out.append(name)
+    for item in uni_lst_temp:
+        if '@' in item:
+            item = item.split('@')[1]
+            if '.edu' in item:
+                item = "@"+item.split('.edu')[0] + '.edu'
+        item = item.strip('"\'').rstrip('.')
+        
+        if item not in ['Central', 'The U', 'nan'] and len(item) > 2 and item not in known_schools:
+            flat_list.append(item)
 
-    known_schools= ['@emory.edu', '@gatech.edu', 'Georgia Institute of Technology', 'gatech']
-    for names in known_schools:
-        if names in out:
-            out.remove(names)
+    counts = Counter(flat_list)
+    sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
+    output = [x[0] for x in sorted_counts]
 
-    out = list(set(out))
-    return ['All']+out
+    return ['All'] + output
 uni_init_lst = get_dropdown_options(df['affiliations'].values.tolist())#['All']#
 aoi_init_lsr = get_dropdown_options(df['subjects'].values.tolist())#['All']#
 def fetch_data(university, area_of_interest):
@@ -74,7 +70,6 @@ def fetch_data(university, area_of_interest):
         university = ['All']
     if area_of_interest == None or area_of_interest == []:
         area_of_interest = ['All']
-    
     df1 = df.copy()
     if 'Georgia Tech' in university:
         university.append('Georgia Institute of Technology')
@@ -102,10 +97,18 @@ def generate_initial_layout(university=[], area_of_interest=[]):
     initial_list = []
     for i, entry in enumerate(data):
         new_entry = html.Div([
-            html.A(entry['title'], href=entry['url'], id={'type': 'url-link', 'index': i}, className='url-link'),
-            dcc.Markdown(entry['abstract'], className='url-abstract', mathjax=False,),
+            html.Div(
+                html.A(entry['title'],href=entry['url'],id={'type': 'url-link', 'index': i},className='url-link', style={"text-align": "center", "font-weight": "bold"}
+                ),style={"text-align": "center"}),
+            dcc.Markdown(entry['abstract'], className='url-abstract', mathjax=True,),
+            html.Div([
+        html.Div([
             html.Button('Like', id={'type': 'like-button', 'index': i}, className='btn btn-success'),
-            html.Button('Dislike', id={'type': 'dislike-button', 'index': i}, className='btn btn-danger',style={"margin-left": "15px"})
+            html.Button('Dislike', id={'type': 'dislike-button', 'index': i}, className='btn btn-danger', style={"margin-left": "15px"}),
+        ], style={"display": "inline-block"}),
+        dcc.Markdown(entry['source'], className='url-source', mathjax=False,),
+    ], style={"display": "flex", "justify-content": "space-between", "align-items": "center"})
+
         ], className='url-entry list-group-item', style={'marginBottom':'1%', 'marginRight':'1%', 'marginLeft':'1%'})
         initial_list.append(new_entry)
     return initial_list
@@ -119,10 +122,8 @@ app._favicon = (r"assets\favicon.ico")
     [Input('interval-component', 'n_intervals'),]
 )
 def get_img_url(value):#this both updates the DF on log in just in case
-    global df
-    df = pd.read_pickle('last_month.pkl') #de_duped, osf_data
     try:
-        user = request.authorization['username']
+        user =request.authorization['username']
     except:
         user = "None"
     return UNI_LOGO_URL[USER_GROUPS[user]]
@@ -137,44 +138,49 @@ def update_dropdowns(noop):
     return universitys, areas_of_interest
 app.layout = html.Div([html.Br(),
     html.Img(id ='uni_logo' , style={
-      "height": "20%",
-      "width": "20%",
+      "height": "10%",
+      "width": "10%",
       "object-fit": "contain",
-      "margin-left": '29%',
-      'margin-right':'15px'
+      "margin-left": '39%',
+      'margin-right':'5px'
     }),
     html.Img(src ='https://upload.wikimedia.org/wikipedia/commons/c/ca/Transparent_X.png?20130727130546' , style={
       "height": "1%",
       "width": "1%",
       "object-fit": "contain",
-      "margin-left": "15px"
+      "margin-left": "5px"
     }),
     html.Img(src=app.get_asset_url('research_finder_logo.jpg'), style={
-      "height": "20%",
-      "width": "20%",
+      "height": "10%",
+      "width": "10%",
       "object-fit": "contain",
-      "margin-left": "15px"
-    }), html.Br(),html.Br(),html.Br(),html.H2(id='show-output', children='',style={"margin-left": "2%"}), html.Br(),html.Br(),
+      "margin-left": "5px"
+    }),html.H2(id='show-output', children='',style={"margin-left": "2%"}), html.Br(),
     
     
     # Search bar for "University"
     html.Div([
-        html.H2("University:",style={"margin-left": "2%"}),
-        dcc.Dropdown(id='university-input', options=uni_init_lst, value=['All'], placeholder='All available Universities' ,multi=True,style={"margin-left": "1%", "margin-right": "3%", })
-    ]),
-    html.Br(),html.Br(),
     html.Div([
-        html.H2("Area of Interest:",style={"margin-left": "2%"}),
-        dcc.Dropdown(id='area-of-interest-input', options=['All'], value=['All'], placeholder='All available Areas of Interest' , multi=True,style={"margin-left": "1%", "margin-right": "3%", })#"margin-left": "2%", "margin-right": "4%"
-    ]),html.Br(),html.Br(),
+        html.H2("University:", style={"margin-left": "4%"}),
+        dcc.Dropdown(id='university-input', options=uni_init_lst, value=['All'], placeholder='All available Universities', multi=True, style={"margin-left": "2%", "margin-right": "3%"})
+    ], style={"display": "inline-block", "width": "45%"}),
+    html.Div([
+        html.H2("Area of Interest:", style={"margin-left": "6%"}),
+        dcc.Dropdown(id='area-of-interest-input', options=['All'], value=['All'], placeholder='All available Areas of Interest', multi=True, style={"margin-left": "3%", "margin-right": "3%"})
+    ], style={"display": "inline-block", "width": "45%"})
+])
+,
+    
+    html.Br(),
     html.Div(
         id='url-list',
         children=generate_initial_layout(),
         className='list-group'
     ),
+    html.H4("© Ethan Povlot 2023", style={"margin-left": "10%"}),html.Br(),
     # Hidden div to keep track of clicked URLs
     html.Div(id='clicked-urls', style={'display': 'none'}),
-    dcc.Interval(id='interval-component', interval=10, n_intervals=1, max_intervals=2)
+    dcc.Interval(id='interval-component', interval=10, n_intervals=1, max_intervals=1)
     
 ], style={
     'backgroundColor': '#9fe7e7',
@@ -206,8 +212,6 @@ def update_url_list(university, area_of_interest):
 def update_output_div(n_clicks):
     logged_username = request.authorization['username']
     if n_clicks:
-        global username
-        username = logged_username
         user_group_to_name = {'Emory':'Emory University', 'GT':'Georgia Tech'}
         return '  Hello '+logged_username+', welcome to Research Finder', [user_group_to_name[USER_GROUPS[logged_username]]]
     else:
@@ -277,5 +281,12 @@ if __name__ == '__main__':
     #cmd ipconfig them
     #Wireless LAN adapter Wi-Fi:  IPv4 Address. . . . . . . . . . . : 10.91.6.65 so thus is the host
     #host='10.91.6.65',
-        app.run_server(host='10.91.125.61', port='8050', debug=True)
-
+        app.run_server(host='128.61.105.126', port='80', debug=False)
+#       serve(app, host='10.91.125.61', port=80, url_scheme='https')
+# add filter for which sources
+# flexible filtering
+# add columns for search dropdowns
+# add dates as a filter
+#maybe advaced search for dates and full search
+#maybe pagination
+# store last date used to compute score as num days that they looked at not just num days ago
